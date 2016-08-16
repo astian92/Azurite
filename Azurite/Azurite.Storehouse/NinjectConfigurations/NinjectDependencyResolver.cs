10f0d2f33f4fcecc796077c5fe60a974c1f38158
astian <abrashev92@gmail.com>
2016-08-16 21:15:48 +0300

    ninject config

---
+++ Azurite/Azurite.Storehouse/NinjectConfigurations/NinjectDependencyResolver.cs
@@ -0,0 +1,45 @@
+﻿using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Azurite.Storehouse.NinjectConfigurations
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private readonly IKernel _kernel;
+
+        public NinjectDependencyResolver()
+        {
+            _kernel = new StandardKernel();
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return _kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return _kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+            //_kernel.Bind<IDbFactory>()
+            //    .To<DbFactory>()
+            //    .InTransientScope();
+
+            //_kernel.Bind<ITranslatedRepository<ProductW, Product, Guid>>()
+            //    .To<GenericTranslatedRepository<ProductW, Product, Guid>>()
+            //    .InTransientScope();
+
+            //_kernel.Bind<IRepository<Product, Guid>>()
+            //    .To<IRepository<Product, Guid>>()
+            //    .InTransientScope();
+        }
+    }
+}
