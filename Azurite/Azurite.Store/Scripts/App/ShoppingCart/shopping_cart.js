4b5256ddd033afa6113917f00cba8adc79b0b5c9
bork0o <bork0o0131@gmail.com>
2016-12-11 17:16:52 +0200

    work on shopping CartSummary

--- Azurite/Azurite.Store/Scripts/App/ShoppingCart/shopping_cart.js
+++ Azurite/Azurite.Store/Scripts/App/ShoppingCart/shopping_cart.js
@@ -1,9 +1,18 @@
 ﻿function loadShoppingCart() {
+    //$.ajax({
+    //    url: MVC.ShoppingCart.ProductsFull,
+    //    dataType: 'html',
+    //    success: function (data) {
+    //        $('#cart-products').html(data);
+    //    }
+    //});
+
+    $('#cart-summary').html('');
     $.ajax({
-        url: MVC.ShoppingCart.ProductsFull,
+        url: MVC.ShoppingCart.CartSummaryFull,
         dataType: 'html',
         success: function (data) {
-            $('#cart-products').html(data);
+            $('#cart-summary').html(data);
         }
     });
 }
