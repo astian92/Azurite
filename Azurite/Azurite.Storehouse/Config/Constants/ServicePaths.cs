929e2b9a525992683c511d940d92fb5573b5a403
astian <abrashev92@gmail.com>
2016-12-19 18:24:54 +0200

    settings

--- Azurite/Azurite.Storehouse/Config/Constants/ServicePaths.cs
+++ Azurite/Azurite.Storehouse/Config/Constants/ServicePaths.cs
@@ -2,13 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 
 namespace Azurite.Storehouse.Config.Constants
 {
     public static class ServicePaths
     {
-        public static string CDN = "http://localhost:8696/cdn/";
-        public static string Files = "http://localhost:8696/cdn/files/";
-        public static string Save = "http://localhost:8696/cdn/save/";
+        public static string CDN;
+        public static string Files;
+        public static string Save;
+        public static string Remove;
+
+        static ServicePaths()
+        {
+            string cdn = WebConfigurationManager.AppSettings["CdnAddress"];
+
+            CDN = cdn;
+            Files = cdn + "files/";
+            Save = cdn + "save/";
+            Remove = cdn + "remove/";
+        }
     }
 }
--- Azurite/Azurite.Storehouse/Services/Implementations/CdnService.cs
+++ Azurite/Azurite.Storehouse/Services/Implementations/CdnService.cs
@@ -1,4 +1,5 @@
-﻿using Azurite.Storehouse.Models.Http;
+﻿using Azurite.Storehouse.Config.Constants;
+using Azurite.Storehouse.Models.Http;
 using Azurite.Storehouse.Services.Contracts;
 using System;
 using System.Collections.Generic;
@@ -12,27 +13,27 @@ namespace Azurite.Storehouse.Services.Implementations
 {
     public class CdnService : ICdnService
     {
-        private readonly string cdnAddress;
+        //private readonly string cdnAddress;
         private readonly IHttpService httpService;
         private readonly Guid key;
 
         public CdnService(IHttpService service)
         {
             this.httpService = service;
-            cdnAddress = WebConfigurationManager.AppSettings["CdnAddress"];
+            //cdnAddress = WebConfigurationManager.AppSettings["CdnAddress"];
             string keyStr = WebConfigurationManager.AppSettings["SecurityKey"];
             this.key = Guid.Parse(keyStr);
         }
 
         public async Task<bool> SaveFiles(IEnumerable<HttpFile> files)
         {
-            var result = await httpService.PostAsync(new Uri(cdnAddress + "save"), new { Key = this.key, files = files });
+            var result = await httpService.PostAsync(new Uri(ServicePaths.Save), new { Key = this.key, files = files });
             return result.IsSuccessStatusCode;
         }
 
         public async Task<bool> DeleteFiles(IEnumerable<Guid> fileIds)
         {
-            var result = await httpService.PostAsync(new Uri(cdnAddress + "remove"), new MultipleIds() { Key = this.key, Ids = fileIds });
+            var result = await httpService.PostAsync(new Uri(ServicePaths.Remove), new MultipleIds() { Key = this.key, Ids = fileIds });
             return result.IsSuccessStatusCode;
         }
     }
